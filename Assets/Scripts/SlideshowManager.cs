462b3a7fff92f0e4aa06d0b55f5d02e364ceecbf
gabby-reidy <gabrielle.reidy@g.austincc.edu>
2026-07-31 15:27:31 -0500

    Imported new assets, organized folders, added cutscene scenes
    Imported some temporary assets to test cutscene code.
    Organized all art assets into new folders.
    Created Cutscene scene and SlideshowManager.cs to handle slideshow and transition into next scene.

---
+++ Assets/Scripts/SlideshowManager.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SlideshowManager : MonoBehaviour
+{
+    [SerializeField] private Sprite[] frames;
+    [SerializeField] private Image slide;
+
+    private int currentIndex = 0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GoToNextSlide();
+        }
+    }
+
+    private void GoToNextSlide()
+    {
+        currentIndex++;
+
+        if (currentIndex < frames.Length)
+        {
+            ChangeSlideSprite();
+        }
+        else
+        {
+            SceneManager.LoadScene("LevelOne");
+        }
+    }
+
+    private void ChangeSlideSprite()
+    {
+        slide.sprite = frames[currentIndex];
+    }
+}
