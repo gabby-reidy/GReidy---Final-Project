53f655c5f5bf897cf9f26ebb121ae7c36c600ccf
gabby-reidy <gabrielle.reidy@g.austincc.edu>
2026-08-02 16:59:38 -0500

    Updated EnemySpawner.cs
    Setting up struct and lists for spawning enemies in random places and then setting their patrol points.
    To do list:
    Need to actually test and set everything in inspector & set up active enemy count for the coroutine.
    Make additional enemy prefabs
    Maybe add a boss enemy to spawn when all regular enemies are destroyed

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,15 +1,27 @@
 using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
 
 public class EnemySpawner : MonoBehaviour
 {
-    [SerializeField] private GameObject enemyPrefab;
-    [SerializeField] private Transform spawnPoint;
-    [SerializeField] private Transform[] wayPoints;
+    [System.Serializable]
+    public struct SpawnLocations
+    {
+        public string Name;
+        public Transform SpawnPoint;
+        public Transform[] PatrolPoints;
+    }
+    [SerializeField] private float spawnTimer = 10f;
+    [SerializeField] private int maxEnemies = 5;
+    // List of enemy prefabs - need to add additional enemies to prefab folder 
+    [SerializeField] private List<GameObject> enemyPrefabs = new List<GameObject>();
+    // List of spawn locations for randomization
+    [SerializeField] private List<SpawnLocations> spawnZones = new List<SpawnLocations>();
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        SpawnEnemy();
+        //SpawnEnemy();
     }
 
     // Update is called once per frame
@@ -18,17 +30,24 @@ public class EnemySpawner : MonoBehaviour
 
     }
 
-    private void SpawnEnemy()
+    private IEnumerator SpawnEnemiesOnTimer()
     {
-        if (enemyPrefab == null || spawnPoint == null)
-        {
-            return;
-        }
-        GameObject newEnemy = Instantiate(enemyPrefab, spawnPoint.position, enemyPrefab.transform.rotation);
+        yield return new WaitForSeconds(spawnTimer);
+    }
+
+    private void SpawnRandomEnemy()
+    {
+        int spawnZoneIndex = Random.Range(0, spawnZones.Count);
+        SpawnLocations randomZone = spawnZones[spawnZoneIndex];
+
+        int enemyIndex = Random.Range(0, enemyPrefabs.Count);
+        GameObject randomPrefab = enemyPrefabs[enemyIndex];
+
+        GameObject newEnemy = Instantiate(randomPrefab, randomZone.SpawnPoint.position, randomPrefab.transform.rotation);
         Enemy enemyScript = newEnemy.GetComponent<Enemy>();
         if (enemyScript != null)
         {
-            enemyScript.InitializePatrol(wayPoints);
+            enemyScript.InitializePatrol(randomZone.PatrolPoints);
         }
     }
 }
